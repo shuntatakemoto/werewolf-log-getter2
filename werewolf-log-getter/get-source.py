f8fe786171c52199da137fb47f792e4a3589d73b
shuntatakemoto <rt80405@gmail.com>
2022-05-16 18:36:29 +0900

    負荷対策

--- werewolf-log-getter/get-source.py
+++ werewolf-log-getter/get-source.py
@@ -1,10 +1,14 @@
 import requests
+import time
 
-for id in range(799930, 799936):
+# 取得範囲を設定(x,y=int)
+for id in range(x, y):
     url = 'https://zinro.net/m/log.php?id='+str(id)
     filename = 'log/log-'+str(id)+".txt"
 
     urlData = requests.get(url).content
 
     with open(filename, mode='wb') as f:  # wb でバイト型を書き込める
+        # 負荷対策(t=int)
+        time.sleep(t)
         f.write(urlData)
