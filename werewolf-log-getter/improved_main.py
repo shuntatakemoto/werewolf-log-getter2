ffb5e93e9ccd0c6d2870f97dd4168dc598533936
haruta <58549977+shuntatakemoto@users.noreply.github.com>
2022-07-03 17:29:42 +0900

    30111対策

--- werewolf-log-getter/improved_main.py
+++ werewolf-log-getter/improved_main.py
@@ -24,23 +24,24 @@ for i in range(1, 31):
     if len(str)!=0:
         srt_array_deleted_forward=str_array[18:]
         str_array_deleted_back=srt_array_deleted_forward[:-2]
-        if is_eliminated==-1:
-            b=eval(str_array_deleted_back)
-            n = len(b)
+        if len(str_array_deleted_back) !=0:
+            if is_eliminated==-1:
+                b=eval(str_array_deleted_back)
+                n = len(b)
 
-            for j in range(n):
-                message_data.append([j, b[j]["from_user"], "->", b[j]
-                                        ["to_user"], b[j]["job"], b[j]["message"]])
-                if b[j]["message"] == white_win:
-                    winner = "村人"
-                elif b[j]["message"] == black_win:
-                    winner = "人狼"
+                for j in range(n):
+                    message_data.append([j, b[j]["from_user"], "->", b[j]
+                                            ["to_user"], b[j]["job"], b[j]["message"]])
+                    if b[j]["message"] == white_win:
+                        winner = "村人"
+                    elif b[j]["message"] == black_win:
+                        winner = "人狼"
 
-            log_data["log_id"] = i
-            log_data["winner"] = winner
-            log_data["message"] = message_data
+                log_data["log_id"] = i
+                log_data["winner"] = winner
+                log_data["message"] = message_data
 
-        if is_eliminated == -1 and len(str) >= 15000 and winner != "":
-            file_number+=1
-            with open("json/log-{}.json".format(file_number), "w") as f:
-                log_json = json.dump(log_data, f, indent=4, ensure_ascii=False)
+            if is_eliminated == -1 and len(str) >= 15000 and winner != "":
+                file_number+=1
+                with open("json/log-{}.json".format(file_number), "w") as f:
+                    log_json = json.dump(log_data, f, indent=4, ensure_ascii=False)
